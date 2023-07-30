f40838f72c9da23afc1ac03f0a5ee56116dea5a6
Mehmet GÜVEN <guvennmehmet@gmail.com>
2023-07-30 04:45:26 +0300

    userfaces created, styling added, logic started.

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+let edittedPlayer = 0;
+let activePlayer = 0;
+const players = [
+  { name: "", symbol: "X" },
+  { name: "", symbol: "O" },
+];
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameAreaElement = document.getElementById("active-game");
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const gameFieldElements = document.querySelectorAll("#game-board li");
+//const gameBoardElement = document.getElementById("game-board");
+const activePlayerNameElement = document.getElementById("active-player-name");
+
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
+formElement.addEventListener("submit", savePlayerConfig);
+startNewGameBtnElement.addEventListener("click", startNewGame);
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener("click", selectGameField);
+}
+// gameBoardElement.addEventListener("click", selectGameField);
---
+++ scripts/config.js
@@ -0,0 +1,29 @@
+function openPlayerConfig(event) {
+  edittedPlayer = +event.target.dataset.playerid;
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + edittedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+  players[edittedPlayer - 1].name = enteredPlayerName;
+  closePlayerConfig();
+}
